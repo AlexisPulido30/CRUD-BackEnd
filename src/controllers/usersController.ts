f0f2c7eb8661a4e8feef5832d84d1df0b8a6c89e
AlexisPulido30 <alexispulido.agn@outlook.com>
2026-08-18 11:42:29 -0600

    feat: modificacion de consulta para traer usuarios activos

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -4,11 +4,29 @@ import { prisma } from "../config/db";
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const users = await prisma.user.findMany({ where: { activo: true }});
+    const users = await prisma.user.findMany({
+      where: { activo: true },
+      select: {
+        id: true,
+        nombre: true,
+        correo: true,
+        telefono: true,
+        activo: true,
+        fechaNacimiento: true,
+        genero: true, 
+        role: {
+          select: {
+            id: true,
+            nombre: true,
+          },
+        },
+      },
+      orderBy: { nombre: "asc" },
+    });
 
-    res.json(users);
+    return res.json(users);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error al obtener los usuarios" });
+    return res.status(500).json({ error: "Error al obtener los usuarios" });
   }
 };
